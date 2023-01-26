97315aa16cebeb07a13827af5582107c1af9c1e4
MikEKun0 <lavlinskiy2001@mail.ru>
2023-01-27 00:15:01 +0300

    remove useless and add test

--- simdy/test/Spec.hs
+++ simdy/test/Spec.hs
@@ -1,2 +1,22 @@
+module Main (main)  where
+import Parser (parseToplevel)
+import Syntax ( Op(..), Expr(..), Type(..), PrimitiveType(..),Name, UnaryOp(..), MemoryOp(..) )
+import Test.Tasty
+import Test.Tasty.HUnit as H
+
+testBinOp:: TestTree
+testBinOp = 
+    let
+        input = "4 == 4"
+        expr = [BinOp Equal (Number 4) (Number 4)]
+    in 
+        testCase "AST: parse expression 4 == 4" $ assertEqual [] True (expr == parseToplevel input)
+
+tests :: TestTree
+tests = testGroup "Unit tests: Approximation" 
+        [ 
+            testGroup "Test group: AST" [testBinOp]
+        ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
